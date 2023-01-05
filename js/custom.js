a3fea9ce4b1888163321c327fe8a6f9ed2fc2ef0
viktoriia-kravchuk <vikakravchuk2@gmail.com>
2023-01-05 14:40:35 +0100

    blog

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,29 @@
 /** INSERT CUSTOM JS HERE **/
-export default function() {
 
+const modal = document.getElementById("modal");
+const backdrop = document.querySelector(".backdrop");
+const play = document.querySelector(".hero__btn");
+const close = document.querySelector(".close_modal");
+
+const body = document.body;
+const html = document.documentElement;
+
+
+export default function () {
+
+  play.addEventListener("click", () => {
+    modal.classList.add("open");
+    backdrop.classList.add("open");
+    html.style.overflowY =
+      html.style.overflowY === "hidden" ? "visible" : "hidden";
+    body.style.overflowY = body.style.overflowY === "hidden" ? "visible" : "hidden";
+  });
+
+  close.addEventListener("click", () => {
+    modal.classList.remove("open");
+    backdrop.classList.remove("open");
+    html.style.overflowY =
+      html.style.overflowY === "hidden" ? "visible" : "hidden";
+    body.style.overflowY = body.style.overflowY === "hidden" ? "visible" : "hidden";
+  });
 }
